ce267470c77ee24dd6115c1f5743022687287432
Antonio Alberto Giannalia <antonio.giannalia@truelink.it>
2025-05-04 12:41:02 +0200

    Add health check

--- main.go
+++ main.go
@@ -9,9 +9,15 @@ import (
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	fileServer := http.FileServer(http.Dir(filepathRoot))
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 
 	srv := &http.Server{
 		Addr:    ":" + port,
